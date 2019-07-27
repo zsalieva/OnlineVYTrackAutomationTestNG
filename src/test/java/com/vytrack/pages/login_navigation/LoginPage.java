bb18e7b229f150765164b8aeb816cca640afe8da
CybertekSchool <github17>
2019-07-27 15:03:35 -0400

    Login page tests

--- src/test/java/com/vytrack/pages/login_navigation/LoginPage.java
+++ src/test/java/com/vytrack/pages/login_navigation/LoginPage.java
@@ -2,12 +2,42 @@ package com.vytrack.pages.login_navigation;
 
 import com.vytrack.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+    //we are
     private WebDriver driver = Driver.getDriver();
+
+    @FindBy(id = "prependedInput")
+    public WebElement userNameElement;
+
+    @FindBy(name = "_password")
+    public WebElement passwordElement;
+
+    @FindBy(id="_submit")
+    public WebElement loginButtonElement;
+
+    @FindBy(id = "remember_me")
+    public WebElement rememberMeElement;
+
+    @FindBy(partialLinkText = "Forgot your password?")
+    public WebElement forgotPasswordElement;
+
+    @FindBy(tagName = "h2")
+    public WebElement titleElement;
+
+    @FindBy(css = "[class='alert alert-error'] > div")
+    public WebElement errorMessageElement;
 
     public LoginPage(){
         PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password){
+        userNameElement.sendKeys(username);
+        passwordElement.sendKeys(password);
+        loginButtonElement.click();
     }
 }
--- src/test/java/com/vytrack/tests/components/login_navigation/LoginTests.java
+++ src/test/java/com/vytrack/tests/components/login_navigation/LoginTests.java
@@ -1,4 +1,18 @@
 package com.vytrack.tests.components.login_navigation;
 
-public class LoginTests {
+import com.vytrack.pages.login_navigation.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void loginTest1(){
+        String username = ConfigurationReader.getProperty("storemanagerusername");
+        String password = ConfigurationReader.getProperty("storemanagerpassword");
+        loginPage.login(username, password);
+    }
 }
